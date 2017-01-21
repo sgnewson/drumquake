6527eac2d7b5f28a542f3210e5dd81b095eebc0b
Aidan Waite <aidanaidan@gmail.com>
2017-01-21 13:26:29 -0700

    Light feedback on drum presssssssssss

--- Assets/Scripts/DrumControlMaster.cs
+++ Assets/Scripts/DrumControlMaster.cs
@@ -6,23 +6,28 @@ using XboxCtrlrInput;
 public class DrumControlMaster : MonoBehaviour {
 
 	public WaveManager WaveManager;
+	public Light FeedbackLight;
 
 	void Update () {
 
 		if (XCI.GetButtonDown (XboxButton.B) == true || Input.GetKeyDown(KeyCode.Alpha1)) {
 			WaveManager.HandleDrumPress(XboxButton.B);
+			FeedbackLight.color = Color.red;
 		}
 
 		if (XCI.GetButtonDown (XboxButton.Y) == true || Input.GetKeyDown(KeyCode.Alpha2)) {
 			WaveManager.HandleDrumPress(XboxButton.Y);
+			FeedbackLight.color = Color.yellow;
 		}
 
 		if (XCI.GetButtonDown (XboxButton.X) == true || Input.GetKeyDown(KeyCode.Alpha3)) {
 			WaveManager.HandleDrumPress(XboxButton.X);
+			FeedbackLight.color = Color.blue;
 		}
 
 		if (XCI.GetButtonDown (XboxButton.A) == true || Input.GetKeyDown(KeyCode.Alpha4)) {
 			WaveManager.HandleDrumPress(XboxButton.A);
+			FeedbackLight.color = Color.green;
 		}
 	}
 }
