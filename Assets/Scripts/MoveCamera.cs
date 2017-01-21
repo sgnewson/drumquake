710320435b3a14cb22f8d3ca73b49ee51dc3ce66
Scott Newson <scott.g.newson@gmail.com>
2017-01-21 16:09:21 -0700

    add camera shake

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -2,15 +2,44 @@
 
 public class MoveCamera : MonoBehaviour {
     
+
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+	public float shake_decay;
+	public float shake_intensity;
+
     public float speed = 0.1f;
 
-	// Update is called once per frame
 	void Update () {
-        var y = Input.GetAxis("Vertical");
-        var x = Input.GetAxis("Horizontal");
-
-        var delta = new Vector3(x, y, 0);
-
-        this.transform.Translate(delta * speed);
+		UpdateCameraPosition ();
+		UpdateCameraShake ();
 	}
+
+	void UpdateCameraPosition ()
+	{
+		var y = Input.GetAxis ("Vertical");
+		var x = Input.GetAxis ("Horizontal");
+		var delta = new Vector3 (x, y, 0);
+		this.transform.Translate (delta * speed);
+	}
+
+	void UpdateCameraShake ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Shake ();
+		}
+		if (shake_intensity > 0) {
+			transform.position = originPosition + Random.insideUnitSphere * shake_intensity;
+			transform.rotation = new Quaternion (originRotation.x + Random.Range (-shake_intensity, shake_intensity) * .2f, originRotation.y + Random.Range (-shake_intensity, shake_intensity) * .2f, originRotation.z + Random.Range (-shake_intensity, shake_intensity) * .2f, originRotation.w + Random.Range (-shake_intensity, shake_intensity) * .2f);
+			shake_intensity -= shake_decay;
+		}
+	}
+
+	void Shake(){
+		originPosition = transform.position;
+		originRotation = transform.rotation;
+		shake_intensity = .3f;
+		shake_decay = 0.002f;
+	}
+
 }
