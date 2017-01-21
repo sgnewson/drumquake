6b5bef42ee7c9d14a4f9d2b2f7f47e12d9bde36d
Aidan Waite <aidanaidan@gmail.com>
2017-01-20 21:06:00 -0700

    Initial waves spawning and shrinking

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave {
+
+	public float Percentage;
+	public GameObject WavePrefab;
+
+	public Wave(float percentage, GameObject prefab) {
+		Percentage = percentage;
+		WavePrefab = prefab;
+	}
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+	public GameObject WavePrefab;
+	public float DelayBetweenWaves;
+	public List<Wave> WaveList;
+
+	public float WaveDurationSeconds;
+	float currentTime;
+
+	public enum LerpMode {Linear, EaseOut, EaseIn, Exponential, SmoothStep, SmootherStep}
+	public LerpMode TransitionLerpMode;
+
+	void Awake() {
+		WaveList = new List<Wave> ();
+		StartCoroutine ("SpawnWave");
+	}
+
+	IEnumerator SpawnWave() {
+		yield return new WaitForSeconds (DelayBetweenWaves);
+
+		GameObject newWaveGameObject = (GameObject) GameObject.Instantiate (WavePrefab, new Vector3 (0f, 0f, 0f), Quaternion.identity);
+		Wave newWave = new Wave (1f, newWaveGameObject);
+
+		WaveList.Add (newWave);
+
+		StartCoroutine ("SpawnWave");
+	}
+
+	void Update() {
+
+		foreach (Wave wave in WaveList) {
+			float per = wave.Percentage;
+			if (per <= 0f) {
+				// Wave is in the middle so destroy it
+
+			} else {
+				wave.Percentage -= Time.deltaTime;
+				wave.WavePrefab.transform.localScale = new Vector3 (wave.Percentage, wave.Percentage, wave.Percentage);
+			}
+		}
+
+	}
+}
