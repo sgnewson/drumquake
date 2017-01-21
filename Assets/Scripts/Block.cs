a96d4d10510cbf7ea54e6ea6eb99c24b33bfcbd8
T Henders <trevmeister42@gmail.com>
2017-01-21 00:30:58 -0700

    Added grid-snapping and locking of blocks as soon as they touch another block.  Also, collisions with surfaces stop movement in that direction.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,7 +6,11 @@ public class Block : MonoBehaviour
 {
     private Vector3 screenPoint;
     private Vector3 offset;
-
+    private bool isColliding = false;
+    private ContactPoint[] contacts;
+    private enum Faces { TOP, BOTTOM, LEFT, RIGHT };
+    private bool[] canMove = { true, true, true, true };
+    private bool locked = false;
 
     void OnMouseDown()
     {
@@ -16,24 +20,105 @@ public class Block : MonoBehaviour
 
     void OnMouseDrag()
     {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        if (!locked)
+        {
+            if (!isColliding)
+            {
+                Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
+                Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+                // Reassigns curPosition to rounded values for grid-snapping.
+                curPosition = new Vector3(Mathf.Round(curPosition.x), Mathf.Round(curPosition.y), Mathf.Round(curPosition.z));
+                transform.position = curPosition;
+            }
+            else
+            {
+                foreach (ContactPoint contact in contacts)
+                {
+                    if (contact.normal.x == 1)
+                    {
+                        canMove[(int)Faces.LEFT] = false;
+                    }
+                    else if (contact.normal.x == -1)
+                    {
+                        canMove[(int)Faces.RIGHT] = false;
+                    }
+                    else if (contact.normal.y == 1)
+                    {
+                        canMove[(int)Faces.BOTTOM] = false;
+                    }
+                    else if (contact.normal.y == -1)
+                    {
+                        canMove[(int)Faces.TOP] = false;
+                    }
+                }
+
+                Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+                Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+                
+                // Reassigns curPosition to rounded values for grid-snapping.
+                curPosition = new Vector3(Mathf.Round(curPosition.x), Mathf.Round(curPosition.y), Mathf.Round(curPosition.z));
+
+                if (!canMove[(int)Faces.LEFT] && curPosition.x < transform.position.x)
+                {
+                    curPosition.x = transform.position.x;
+                }
+
+                if (!canMove[(int)Faces.RIGHT] && curPosition.x > transform.position.x)
+                {
+                    curPosition.x = transform.position.x;
+                }
+
+                if (!canMove[(int)Faces.TOP] && curPosition.y > transform.position.y)
+                {
+                    curPosition.y = transform.position.y;
+                }
+
+                if (!canMove[(int)Faces.BOTTOM] && curPosition.y < transform.position.y)
+                {
+                    curPosition.y = transform.position.y;
+                }
+
+                transform.position = curPosition;
+            }
+        }
     }
 
-    int xCoord;
-    int yCoord;
+    private void OnCollisionEnter(Collision collision)
+    {
+        contacts = collision.contacts;
+        isColliding = true;
 
-	// Use this for initialization
-	void Start ()
+        if (collision.collider.gameObject.GetComponent<Block>() != null)
+        {
+            locked = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        isColliding = false;
+        
+        for (int iii = 0; iii < canMove.Length; iii++)
+        {
+            canMove[iii] = true;
+        }
+    }
+
+
+    int row;
+    int col;
+
+    // Use this for initialization
+    void Start()
     {
-		
-	}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
