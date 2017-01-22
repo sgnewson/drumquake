536edf715ddb804d57281c6848b2ece1a6ef9dc2
T Henders <trevmeister42@gmail.com>
2017-01-22 15:58:42 -0700

    Updated credits to support 4:3 and added Shae.

---
+++ Assets/Scripts/Stop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y <= 0)
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
