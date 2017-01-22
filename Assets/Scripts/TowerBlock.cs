15ae38606d25bd2ab7f388ccacb93b01d4e5daf2
Arrain <ser_arrain@yahoo.com>
2017-01-21 20:53:38 -0700

    Glue functionality (3x3 pending) and menu background material

---
+++ Assets/Scripts/TowerBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBlock : MonoBehaviour
+{
+    public class GroupEntity
+    {
+        public List<TowerBlock> memberBlocks;
+    }
+
+    public int gridX { get; set; }
+    public int gridY { get; set; }
+    public int type { get; set; }
+
+    public GroupEntity blockGroup { get; set; }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
